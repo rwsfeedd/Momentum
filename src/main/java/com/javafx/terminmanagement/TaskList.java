62253286fdc6e898cae83b70a3ca75590a5899ab
Samuel B <sbiebsam@gmail.com>
2025-07-04 12:49:13 +0200

    started implementing TaskList

---
+++ src/main/java/com/javafx/terminmanagement/TaskList.java
@@ -0,0 +1,14 @@
+package com.javafx.terminmanagement;
+
+import java.util.LinkedList;
+
+public class TaskList {
+    private String name;
+    private LinkedList<Task> allTasksList = new LinkedList<>();
+
+    public TaskList(String name) {
+        this.name = name;
+    }
+
+    public add
+}
