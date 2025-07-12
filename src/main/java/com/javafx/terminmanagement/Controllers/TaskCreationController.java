1969547f40b5278cd1aec296326ec88b9ceea5a1
Samuel B <sbiebsam@gmail.com>
2025-07-12 17:02:43 +0200

    implemented rough Taskcreation

--- src/main/java/com/javafx/terminmanagement/Controllers/TaskCreationController.java
+++ src/main/java/com/javafx/terminmanagement/Controllers/TaskCreationController.java
@@ -2,16 +2,54 @@ package com.javafx.terminmanagement.Controllers;
 
 import com.javafx.terminmanagement.Model;
 import com.javafx.terminmanagement.StartApplication;
+import com.javafx.terminmanagement.Task;
+import com.javafx.terminmanagement.TaskList;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class TaskCreationController {
+    @FXML
+    TextField textfieldName;
+    @FXML
+    RadioButton buttonSetActive;
+    @FXML
+    RadioButton buttonSetInactive;
+    ToggleGroup groupButtonsActive;
+
+    public void initialize() {
+        //ToggleGroup isnt properly set up -> only one button should be accepted
+        groupButtonsActive = new ToggleGroup();
+        System.out.println("ToggleGroup instantiated: " + groupButtonsActive.toString());
+
+        RadioButton buttonSetActive = new RadioButton();
+        buttonSetActive.setToggleGroup(groupButtonsActive);
+        buttonSetActive.setSelected(true);
+        System.out.println("RadioButtonActive set: " + buttonSetActive.getToggleGroup().toString());
+
+        RadioButton buttonSetInactive = new RadioButton();
+        buttonSetInactive.setToggleGroup(groupButtonsActive);
+        System.out.println("RadioButtonInactive set: " + buttonSetInactive.getToggleGroup().toString());
+
+    }
 
     @FXML
     protected void onTaskCreateButtonClick() {
+        Model model = Model.getInstance();
+        Task task = new Task(textfieldName.getCharacters().toString(), true);
+        model.writeTask(task);
 
+        //Testing
+        TaskList current = model.getCurrentTasks();
+        for (int i = 0; i < current.size(); i++) {
+            System.out.println(current.getTask(i).toString());
+        }
+
+        //Wie werden Sonderzeichen wie ö,ä und ü im Name gespeichert
+        //System.out.println(textfieldName.getCharacters().toString());
     }
 
     /**
